052e03e47d1478dc0e82da5f3bf9d5738efe22be
Harshit Toky <harshit.t@hashbytestudio.com>
2025-05-22 23:30:17 +0530

    Friends

--- src/pages/socket.js
+++ src/pages/socket.js
@@ -1,7 +1,11 @@
 // src/socket.js
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000', {
+// const socket = io('http://localhost:5000', {
+//   withCredentials: true,
+// });
+
+const socket = io('https://geo-tracker-backend.onrender.com', {
   withCredentials: true,
 });
 
